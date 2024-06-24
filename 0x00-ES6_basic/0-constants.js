598894bbfe700c8a138c4b910a2f9fbc0baa1a78
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-06-24 15:43:14 +0000

    test

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,23 @@
-export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
-  }
-  
-  export function getLast() {
-    return ' is okay';
-  }
-  
-  export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
-  
-    return combination;
-  }
+export function taskFirst () {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+/**
+   * Returns a string indicating that sometimes let is okay.
+   * @returns {string} The last part of the combination.
+   */
+export function getLast () {
+  return ' is okay';
+}
+
+/**
+   * Combines a base string with the result of getLast().
+   * @returns {string} The combined string.
+   */
+export function taskNext () {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
