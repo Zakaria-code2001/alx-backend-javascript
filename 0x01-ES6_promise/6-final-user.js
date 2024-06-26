69d05dfe082c74130a28442a0e36b135bf2d129c
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-06-26 17:06:36 +0000

    6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = (firstName, lastName, fileName) => Promise.all([
-  signUpUser(firstName, lastName),
-  uploadPhoto(fileName),
-])
-  .then(([userResult, photoResult]) => [
-    { status: 'fulfilled', value: userResult },
-    { status: 'fulfilled', value: photoResult },
-  ])
-  .catch((error) => [
-    { status: 'rejected', value: error.message },
-    { status: 'rejected', value: error.message },
-  ]);
-
-export default handleProfileSignup;
+export default function handleProfileSignup(firstName, lastName, photo) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(photo)])
+    .then((results) => results.map(({ status, value, reason }) => ({
+      status,
+      value: status === 'fulfilled' ? value : reason.toString(),
+    })));
+}
