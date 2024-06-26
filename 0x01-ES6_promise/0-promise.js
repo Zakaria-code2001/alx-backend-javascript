a7251a47c90c040a3d975486e228394c6e2ad099
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-06-26 15:02:16 +0000

    0.promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+export default function getResponseFromAPI() {
+  /* eslint-disable no-unused-vars */
+  const myPromise = new Promise((resolve, reject) => {
+    const response = true;
+    if (response === true) {
+      resolve(true);
+    }
+  });
+}
