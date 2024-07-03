9e72bfb5f9fb7ac5aecb62047842adcff1be6c36
Zakaria Mohammadi <110720129+Zakaria-code2001@users.noreply.github.com>
2024-07-03 12:32:05 +0000

    task3 complete

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,51 +1,69 @@
 interface DirectorInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workDirectorTasks(): string;
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return "Working from home";
   }
-  
-  interface TeacherInterface {
-    workFromHome(): string;
-    getCoffeeBreak(): string;
-    workTeacherTasks(): string;
+
+  getCoffeeBreak(): string {
+    return "Getting a coffee break";
   }
-  
-  class Director implements DirectorInterface {
-    workFromHome(): string {
-      return "Working from home";
-    }
-  
-    getCoffeeBreak(): string {
-      return "Getting a coffee break";
-    }
-  
-    workDirectorTasks(): string {
-      return "Getting to director tasks";
-    }
+
+  workDirectorTasks(): string {
+    return "Getting to director tasks";
   }
-  
-  class Teacher implements TeacherInterface {
-    workFromHome(): string {
-      return "Cannot work from home";
-    }
-  
-    getCoffeeBreak(): string {
-      return "Cannot have a break";
-    }
-  
-    workTeacherTasks(): string {
-      return "Getting to work";
-    }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return "Cannot work from home";
   }
-  
-  const createEmployee = (salary: string | number): Teacher | Director => {
-    if (typeof salary === "number" && salary < 500) {
-      return new Teacher();
-    } else {
-      return new Director();
-    }
+
+  getCoffeeBreak(): string {
+    return "Cannot have a break";
   }
-  
-  console.log(createEmployee(400));
-  console.log(createEmployee(600));
-  console.log(createEmployee("600"));
+
+  workTeacherTasks(): string {
+    return "Getting to work";
+  }
+}
+
+const createEmployee = (salary: string | number): Teacher | Director => {
+  if (typeof salary === "number" && salary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
+
+const isDirector = (employee: any): employee is Director => {
+  return employee instanceof Director;
+};
+
+const executeWork = (employee: Director | Teacher) => {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+};
+
+type Subjects = "Math" | "History";
+
+const teachClass = (todayClass: Subjects): string => {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
+  }
+};
